724dc96107c56b074548004de06024ce47c5ac5c
eebrahimjoy <eebrahimjoy@gmail.com>
2025-05-28 22:25:20 +0400

    updated

--- lib/core/utils/functions.dart
+++ lib/core/utils/functions.dart
@@ -1,10 +1,12 @@
+import 'package:loyapy/core/constants/app_contents.dart';
 import 'package:loyapy/core/core.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void openWhatsApp(String productTitle) async {
   final message =
-  Uri.encodeComponent("Hi, I'm interested in buying: $productTitle");
-  final url = Uri.parse("https://wa.me/+971557983424?text=$message");
+      Uri.encodeComponent("Hi, I'm interested in buying: $productTitle");
+  final url =
+      Uri.parse("https://wa.me/${AppContents.companyPhone}?text=$message");
 
   if (await canLaunchUrl(url)) {
     await launchUrl(url, mode: LaunchMode.externalApplication);
